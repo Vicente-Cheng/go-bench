389198a7ff6834753a785a7cf841790571d9197a
Vicente Cheng <freeze.bilsted@gmail.com>
2017-01-26 15:31:40 +0800

    fs: add some directory operations

---
+++ fs/dir.go
@@ -0,0 +1,32 @@
+package dir
+
+import (
+	"os"
+	"syscall"
+	"time"
+)
+
+// In some cases, umask would affect the folder permissions.
+// We should reset the umask to ensure our permissions.
+func Dir_Create(dirpath string) (error, time.Duration) {
+	syscall.Umask(0) // reset umask
+	t_start := time.Now()
+	err := os.MkdirAll(dirpath, 0777)
+	t_end := time.Now()
+	t_dur := t_end.Sub(t_start)
+	return err, t_dur
+}
+
+//
+func Dir_Remove(dirpath string) (error, time.Duration) {
+	t_start := time.Now()
+	err := os.RemoveAll(dirpath)
+	t_end := time.Now()
+	t_dur := t_end.Sub(t_start)
+	return err, t_dur
+}
+
+//
+func Dir_Traverse(dirpath string) error {
+	return nil
+}
